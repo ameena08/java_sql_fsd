cf8bc0862948340e305996d6c3b7f7f2d53bcf8f
Ameena K A <75846784+ameena08@users.noreply.github.com>
2023-03-14 22:51:13 +0530

    Add files via upload

--- src/Employee.java
+++ src/Employee.java
@@ -1,31 +1,20 @@
-/** Create an employee Table,employee objects with attribute age,no of years working
- * and salary.Increment salary based on experience.if 1 to 2 years,inc by 2.5%.
- * if 3-6yrs,inc by 5%.    if 6-10%,inc by 10.    above 10,12%
- * 
- */
-package ustbatchno3.sonarcube;
+package ustbatchno3.testNG;
 
 /**
- * @author Administrator
+ * Hello world!
  *
  */
-public class Employee {
-	public int age;
-	public String name;
-	public int exp;
-	public double salary;
-	public Employee(int age, String name, int exp, double salary) {
+public class Employee 
+{
+    private String name;
+    private int age;
+    private String department;
+	public Employee(String name, int age, String department) {
 		super();
-		this.age = age;
 		this.name = name;
-		this.exp = exp;
-		this.salary = salary;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
 		this.age = age;
+		this.department = department;
+		
 	}
 	public String getName() {
 		return name;
@@ -33,25 +22,22 @@ public class Employee {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getExp() {
-		return exp;
+	public int getAge() {
+		return age;
 	}
-	public void setExp(int exp) {
-		this.exp = exp;
+	public void setAge(int age) {
+		this.age = age;
 	}
-	public double getSalary() {
-		return salary;
+	public String getDepartment() {
+		return department;
 	}
-	public void setSalary(double salary) {
-		this.salary = salary;
+	public void setDepartment(String department) {
+		this.department = department;
 	}
 	@Override
 	public String toString() {
-		return "Employee [age=" + age + ", name=" + name + ", exp=" + exp + ", salary=" + salary + "]";
+		return "Employee [name=" + name + ", age=" + age + ", department=" + department + "]";
 	}
-	
-	
-	}
-	
-
-
+    
+    
+}
